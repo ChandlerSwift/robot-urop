22eaf28bbc6e415836c9c311586dc7f43e485f10
Chandler Swift <chandler@chandlerswift.com>
2019-11-24 22:50:24 -0600

    Update data capture script

--- hallway-packet-loss-test/capture_data.py
+++ hallway-packet-loss-test/capture_data.py
@@ -12,7 +12,7 @@ def get_ssid():
 
 def ping():
     try:
-        ping_output=subprocess.run(['ping','-c','1','-W','0.1','ahti.d.umn.edu'],timeout=0.4,stdout=subprocess.PIPE)
+        ping_output=subprocess.run(['ping','-c','1','ahti.d.umn.edu'],timeout=0.15,stdout=subprocess.PIPE)
         ping_ms=ping_output.stdout.decode().split("\n")[1].split()[-2].split("=")[1]
     except:
         return "401"
@@ -28,16 +28,20 @@ try:
         print("%s,%s,%s" % (time_res, ssid_res, ping_res))
         results.append({'time': time_res, 'ssid': ssid_res, 'ping': ping_res})
         time_to_next_ping = last_time + 0.2 - time.time()
-        print(time_to_next_ping)
         if time_to_next_ping < 0:
             time_to_next_ping = 0
         last_time += .2
         time.sleep(time_to_next_ping)
 except KeyboardInterrupt:
-    import csv
-    with open(input('filename to write to: '), 'w', newline='') as csvfile:
-        fieldnames = ['time', 'ssid', 'ping']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for result in results:
-            writer.writerow(result)
+    pass
+
+print("Exporting %s results..." % len(results))
+
+import csv
+filename=input('CSV File to write to:')
+with open(filename+'.csv', 'w', newline='') as csvfile:
+    fieldnames = ['time', 'ssid', 'ping']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for result in results:
+        writer.writerow(result)
